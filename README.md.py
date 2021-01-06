7f47e9a4f324cfe82f71b203dce52a9afdf72212
yjclarelee <yjclarelee@postech.ac.kr>
2021-01-06 09:12:12 +0900

    README of the algorithm

---
+++ README.md.py
@@ -0,0 +1,8 @@
+# K-Means Algorithm
+## Definition
+[Wikipedia]<https://en.wikipedia.org/wiki/K-means_clustering>
+## Functions
+- initialize_centroid()
+- assign_points_to_cluster()
+- update_centroid()
+- calculate_tolerance()
